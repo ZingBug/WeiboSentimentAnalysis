dffd641cb46ab7ff9c1693edd1f051f59684d406
ZingBug <lzh9619@126.com>
2018-08-20 09:31:12 +0800

    关键词改为手动输入

--- WeiboSentimentAnalysis/main.py
+++ WeiboSentimentAnalysis/main.py
@@ -6,17 +6,17 @@ if __name__=="__main__":
 
     username = input('微博用户名：')
     password = input('微博密码：')
+    keyword=input('关键词：')
+    startTime=input('搜索时间（如1970-01-01）：')
 
     login=Login()
     if login.login(username,password):
         #登录成功，开始爬虫数据
         session=login.getSession()#得到登录信息的Session
-        keyword = '华泰'
-        startTime = '2018-08-13'
         interval = '40'
         excelPath = 'data/weibo.xlsx'
-        #cd=CollectData(keyword,startTime,excelPath,session,interval)
-        #cd.start()
+        cd=CollectData(keyword,startTime,excelPath,session,interval)
+        cd.start()
         #爬虫数据结束，开始语义分析
         sa=SemanticAnalysis(startTime,keyword,excelPath)
         sa.snowanalysis()#进行语义分析
